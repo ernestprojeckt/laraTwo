82c4d7ca18ecb89208ec67bfa9bc7c8098e9c3cb
ernestprojeckt <vurvnat337@gmail.com>
2022-10-09 16:14:48 +0200

    laramigra

---
+++ app/Helpers/Enums/OrderStatusesEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helpers\Enums;
+
+/**
+ * Roles Constants
+ */
+enum OrderStatusesEnum: string
+{
+    case InProcess = "In Process";
+    case Paid = "Paid";
+    case Completed = "Completed";
+    case Canceled = "Canceled";
+
+    public static function findByKey(string $key)
+    {
+        return constant("self::$key");
+    }
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Helpers\Enums\RolesEnum;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function scopeAdmin($query)
+    {
+        return $this->getRole($query, 'admin');
+    }
+
+    public function scopeCustomer($query)
+    {
+        return $this->getRole($query);
+    }
+
+    protected function getRole($query, $role = 'customer')
+    {
+        return $query->where(
+            'name',
+            '=',
+            RolesEnum::findByKey(ucfirst($role))->value
+        );
+    }
+}
